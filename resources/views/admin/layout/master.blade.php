d0d8a7d0d5c6d292415a91dbf8dee9f4069469f1
Foysal Ahmed <37664416+foysal-ahmed-shohan@users.noreply.github.com>
2022-11-04 14:35:20 +0600

    logout added

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -50,7 +50,12 @@
                         <a class="nav-link text-white" href="{{route('documentFile.create')}}">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="{{route('documentFile.create')}}">Logout</a>
+{{--                        <a class="nav-link text-white" href="{{route('documentFile.create')}}">Logout</a>--}}
+                        <a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();"><i class="fas fa-power-off"></i> Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </div>
