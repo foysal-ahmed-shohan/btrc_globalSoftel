f4b38f1b34c0762e7706afd5af8dd0f129e456b3
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-11-07 13:26:47 +0600

    fixing

--- resources/views/admin/file/create.blade.php
+++ resources/views/admin/file/create.blade.php
@@ -28,7 +28,7 @@
     </form>
 
         <div class="mt-3 mb-5">
-            <div class="card" style="height: 300px; overflow: auto;">
+            <div style="left: -122px; width: 99%" class="card" style="height: 300px; overflow: auto;">
                 <div class="card-body">
                     <h5 class="card-title border-bottom pb-2 px-0">File History</h5>
                     <div class="mt-3">
