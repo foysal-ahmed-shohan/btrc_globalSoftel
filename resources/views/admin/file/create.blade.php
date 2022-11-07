fbc3f4a291a8bdbe8cc425176fbb2351fe27414f
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-11-07 13:50:19 +0600

    added table

--- resources/views/admin/file/create.blade.php
+++ resources/views/admin/file/create.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layout.master')
 @section('content')
-    <section class="container">
-        <h1 class="mt-5 mb-3">File Uploader Widget</h1>
+<section class="container">
+    <h1 class="mt-5 mb-3">File Uploader Widget</h1>
     <form method="POST" action="{{ route('documentFile.store') }}" enctype="multipart/form-data">
-            @csrf
+        @csrf
         <div class="file-upload-box row gap-3">
             <div class="col-md file-row">
                 <div class="file-upload-sub-box">
@@ -20,33 +20,49 @@
                     <input class="file-description border w-50 p-3" type="time" name="time" id="">
                 </div>
                 <br>
-                <textarea placeholder="Note" class="file-description border w-100 p-3" name="note" id="" cols="30"
-                          rows="5"></textarea>
+                <textarea placeholder="Note" class="file-description border w-100 p-3" name="note" id="" cols="30" rows="5"></textarea>
                 <button type="submit" class="mt-2 login-button w-100">Submit</button>
             </div>
         </div>
     </form>
 
-        <div class="mt-3 mb-5">
-            <div style="left: -122px; width: 99%" class="card" style="height: 300px; overflow: auto;">
-                <div class="card-body">
-                    <h5 class="card-title border-bottom pb-2 px-0">File History</h5>
-                    <div class="mt-3">
-                        @foreach($files as $value)
+    <div class="mt-3 mb-5">
+        <div style="left: -122px; width: 99%" class="card" style="height: 300px; overflow: auto;">
+            <div class="card-body">
+                <h5 class="card-title border-bottom pb-2 px-0">File History</h5>
+                <div class="mt-3">
+                    <!-- @foreach($files as $value)
                          <p class="border-bottom pb-3"><strong class="me-3">{{date('d M, Y', strtotime($value->date))}} {{date('h:s:i A', strtotime($value->time))}}</strong>{{$value->file_original_name}}</p>
-                        @endforeach
-                    </div>
+                        @endforeach -->
+                    <table class="text-dark table">
+                        <thead class="bg-white text-dark">
+                            <tr>
+                                <th>File Name</th>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>Note</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>file.pdf</td>
+                                <td>21/02/2022</td>
+                                <td>10:00 AM</td>
+                                <td>Future file</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-    </section>
-    <div class="w-100">
-        <svg class="lower-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#0B444A" fill-opacity="1"
-                  d="M0,192L26.7,192C53.3,192,107,192,160,197.3C213.3,203,267,213,320,229.3C373.3,245,427,267,480,282.7C533.3,299,587,309,640,282.7C693.3,256,747,192,800,165.3C853.3,139,907,149,960,170.7C1013.3,192,1067,224,1120,218.7C1173.3,213,1227,171,1280,133.3C1333.3,96,1387,64,1413,48L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z">
-            </path>
-        </svg>
     </div>
+</section>
+<div class="w-100">
+    <svg class="lower-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#0B444A" fill-opacity="1" d="M0,192L26.7,192C53.3,192,107,192,160,197.3C213.3,203,267,213,320,229.3C373.3,245,427,267,480,282.7C533.3,299,587,309,640,282.7C693.3,256,747,192,800,165.3C853.3,139,907,149,960,170.7C1013.3,192,1067,224,1120,218.7C1173.3,213,1227,171,1280,133.3C1333.3,96,1387,64,1413,48L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z">
+        </path>
+    </svg>
+</div>
 @endsection
 @section('extra_script')
 <script>
--- resources/views/admin/userActivity/list.blade.php
+++ resources/views/admin/userActivity/list.blade.php
@@ -8,36 +8,34 @@
         <div>
             <table id="myTable" class="text-dark table">
                 <thead class="bg-white text-dark">
-                <tr>
-                    <th>Sl.</th>
-                    <th>User Type</th>
-                    <th>Name</th>
-                    <th>E-mail</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Ip Address</th>
-                    <th>Action</th>
-                </tr>
+                    <tr>
+                        <th>Sl.</th>
+                        <th>User Type</th>
+                        <th>Name</th>
+                        <th>E-mail</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Ip Address</th>
+                        <th>Action</th>
+                    </tr>
                 </thead>
                 <tbody>
 
-                <tr>
-                    <td>1</td>
-                    <td>Nur Tesla</td>
-                    <td>assraf.nur@gmail.com</td>
-                    <td>21 February 2022</td>
-                    <td>21 February 2022</td>
-                    <td>21 February 2022</td>
-                    <td>10:00 AM</td>
-                    <td>
-                        <div>
-                            <a title="Edit" href=""><i class="bi bi-pencil-square text-dark"></i></a>
-                            <a title="Delete" href=""><i class="bi bi-trash3-fill text-dark ms-3"></i></a>
-                        </div>
-                    </td>
-                </tr>
-
-
+                    <tr>
+                        <td>1</td>
+                        <td>Nur Tesla</td>
+                        <td>assraf.nur@gmail.com</td>
+                        <td>21 February 2022</td>
+                        <td>21 February 2022</td>
+                        <td>21 February 2022</td>
+                        <td>10:00 AM</td>
+                        <td>
+                            <div>
+                                <a title="Edit" href=""><i class="bi bi-pencil-square text-dark"></i></a>
+                                <a title="Delete" href=""><i class="bi bi-trash3-fill text-dark ms-3"></i></a>
+                            </div>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
